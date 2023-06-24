afcee461bead44584ffd7ed4fe39a532b6852ff9
MatheuZAguiar <matheuspaguiar099@gmail.com>
2023-06-24 03:45:52 -0300

    Carrinho Vue First Commit

--- eleven-client/src/App.vue
+++ eleven-client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/ContatosComponent">Contatos</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/CarrinhoComponent">Carrinho</router-link>
     <router-view/>
   </nav>
 
@@ -9,24 +9,5 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- eleven-client/src/router/index.ts
+++ eleven-client/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import ContatosComponent from '../components/CONTATOS/ContatosComponent.vue';
+import CarrinhoComponent from '../components/CARRINHO/CarrinhoComponent.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/ContatosComponent',
     name: 'ContatosComponent',
     component: ContatosComponent
+  },
+  {
+    path: '/CarrinhoComponent',
+    name: 'CarrinhoComponent',
+    component: CarrinhoComponent
   }
 ]
 
