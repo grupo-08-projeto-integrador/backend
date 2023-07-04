1e70d0faf6af9bf8fd72107273831cb8ac512804
MatheuZAguiar <matheuspaguiar099@gmail.com>
2023-07-03 23:34:22 -0300

    carrinho semi-pronto

--- FRONTEND/src/views/carrinho/carrinho.view.vue
+++ FRONTEND/src/views/carrinho/carrinho.view.vue
@@ -1,81 +1,113 @@
 <template>
-    <div>
-      <header class="head1"></header>
-      <header class="head2"></header>
-      <div id="hamburguer">
-        <div id="hamburguer-menu" @click="hamburguerOnClick">
-          <div class="line" id="line1"></div>
-          <div class="line" id="line2"></div>
-          <div class="line" id="line3"></div>
-        </div>
-  
-        <nav class="nav" id="nav">
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Carrinho</a></li>
-            <li><a href="#">Contato</a></li>
-            <li><a href="#">Camisetas</a></li>
-            <li><a href="#">Calças</a></li>
-            <li><a href="#">Tênis</a></li>
-          </ul>
-        </nav>
-        <div class="menu-bg" id="menu-bg"></div>
+  <div>
+    <header class="head1"></header>
+    <header class="head2"></header>
+    <div id="hamburguer">
+      <div id="hamburguer-menu" @click="hamburguerOnClick">
+        <div class="line" id="line1"></div>
+        <div class="line" id="line2"></div>
+        <div class="line" id="line3"></div>
       </div>
-  
-      <section class="checkout">        <!--PÁGINA DO CHECK-OUT DOS PRODUTOS-->
-        <h1 id="eleven-checkout">ELEVEN</h1>
-        <h2 id="carrinho-checkout">CARRINHO</h2>
-        <div class="itens-checkout">
-          <span class="item1 cont-item-checkout">
-            <div class="check FGtam2"><input class="checkbox" type="checkbox" checked></div>
-            <div class="item-valor-checkout FGtam3">
-              <p id="produto-checkout">T-Shirt Armani</p>
-              <p id="valor-checkout">Subtotal: R$ 450,00</p>
-            </div>
-            <div class="qtde-checkout FGtam2">
-              <button id="btnmenos-checkout">-</button>
-              <span id="qtdeitens-checkout">
-                <p>Qt: </p>
-                <input type="text" value="1" disabled id="counterValue">
-              </span>
-              <button id="btnmais-checkout">+</button>
-            </div>
-            <button class="remover-checkout FGtam1"><img src="./imagens/clear.png" alt="clear"></button>
-          </span>
-        </div>
-        <div class="precototal-checkout">
-          <div id="tpt-checkout">
-            <p>TOTAL: R$ 450,00</p>
-          </div>
-        </div>
-        <div class="divbtnpf-checkout">
-          <div id="btnFP">
-            <button class="btnFP-checkout">FINALIZAR PEDIDO</button>
-            <p id="aviso">Ao clicar no botão acima, 
-            você será redirecionado(a) para nosso WhatsApp e 
-            nosso atendimento finalizará o seu pedido</p>
-          </div>
-          <div>
-            <button class="btnFP-checkout">REALIZAR CADASTRO</button>
-            <p id="aviso">Ainda não possui cadastro? 
-              Cadastre em nossa Loja Virtual.</p>
-          </div>
-        </div>
-      </section>
+
+      <nav class="nav" id="nav">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Carrinho</a></li>
+          <li><a href="#">Contato</a></li>
+          <li><a href="#">Camisetas</a></li>
+          <li><a href="#">Calças</a></li>
+          <li><a href="#">Tênis</a></li>
+        </ul>
+      </nav>
+      <div class="menu-bg" id="menu-bg"></div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      hamburguerOnClick() {
-        // Implementar a lógica do clique do botão hamburguer
+
+    <section class="checkout">
+      <h1 id="eleven-checkout">ELEVEN</h1>
+      <h2 id="carrinho-checkout">CARRINHO</h2>
+      <div class="itens-checkout">
+        <span v-for="produto in produtosCarrinho" :key="produto.id" class="item1 cont-item-checkout">
+          <div class="check FGtam2"><input class="checkbox" type="checkbox" checked></div>
+          <div class="item-valor-checkout FGtam3">
+            <p id="produto-checkout">{{ produto.nome }}</p>
+            <p id="valor-checkout">Subtotal: R$ {{ produto.subtotal }}</p>
+          </div>
+          <div class="qtde-checkout FGtam2">
+            <button id="btnmenos-checkout" @click="decrementarQuantidade(produto)">-</button>
+            <span id="qtdeitens-checkout">
+              <p>Qt: </p>
+              <input type="text" v-model="produto.quantidade" disabled id="counterValue">
+            </span>
+            <button id="btnmais-checkout" @click="incrementarQuantidade(produto)">+</button>
+          </div>
+          <button class="remover-checkout FGtam1" @click="removerDoCarrinho(produto.id)"><img src="./imagens/clear.png" alt="clear"></button>
+        </span>
+      </div>
+      <div class="precototal-checkout">
+        <div id="tpt-checkout">
+          <p>TOTAL: R$ {{ calcularTotal() }}</p>
+        </div>
+      </div>
+      <div class="divbtnpf-checkout">
+        <div id="btnFP">
+          <button class="btnFP-checkout">FINALIZAR PEDIDO</button>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    produtosCarrinho: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    hamburguerOnClick() {
+      const menu = document.getElementById("nav");
+      const menuBg = document.getElementById("menu-bg");
+      const line1 = document.getElementById("line1");
+      const line2 = document.getElementById("line2");
+      const line3 = document.getElementById("line3");
+
+      menu.classList.toggle("active");
+      menuBg.classList.toggle("active");
+
+      if (menu.classList.contains("active")) {
+        line1.style.transform = "rotate(45deg) translate(4px, 4px)";
+        line2.style.opacity = "0";
+        line3.style.transform = "rotate(-45deg) translate(5px, -5px)";
+      } else {
+        line1.style.transform = "none";
+        line2.style.opacity = "1";
+        line3.style.transform = "none";
+      }
+    },
+    incrementarQuantidade(produto) {
+      produto.quantidade++;
+    },
+    decrementarQuantidade(produto) {
+      if (produto.quantidade > 1) {
+        produto.quantidade--;
+      }
+    },
+    removerDoCarrinho(produtoId) {
+      this.$emit('remover-do-carrinho', produtoId);
+    },
+    calcularTotal() {
+      if (Array.isArray(this.produtosCarrinho)) {
+        return this.produtosCarrinho.reduce((total, produto) => total + produto.subtotal, 0);
+      } else {
+        return 0;
       }
     }
   }
-  </script>
-  
-  <style scoped>
-  @import url('./carrinho.css');
-  </style>
-  
+};
+</script>
+
+<style scoped>
+@import './carrinho.css';
+</style>
--- FRONTEND/src/views/home/produto/produto.view.vue
+++ FRONTEND/src/views/home/produto/produto.view.vue
@@ -1,100 +1,114 @@
 <template>
-    <div class="product-page">
-      <div id="topbar">
-        <div id="divVoltar">
-          <button id="botaoimg" @click="goBack">
-            <img id="pngtop" src="imagens/LeftVoltar.png">
-          </button>
-        </div>
-        <div id="divELEVEN">
-          <h1 id="h1ELEVEN">ELEVEN</h1>
-        </div>
-        <div id="divCarrinho">
-          <button id="botaoimg" @click="goToCart">
-            <img id="pngtop" src="imagens/shoppingCarrinho.png">
-          </button>
-        </div>
+  <div class="product-page">
+    <div id="topbar">
+      <div id="divVoltar">
+        <button id="botaoimg" @click="goBack">
+          <img id="pngtop" src="imagens/LeftVoltar.png">
+        </button>
       </div>
-      <div id="line4"></div>
-      <div id="divsearch">
-        <input type="text" aria-label="Realizar Busca" autocomplete="off" name="Ntt" v-model="searchQuery" class="input" placeholder="O Que Você Procura ?">
-        <button id="searchButton" @click="searchProduct">Buscar</button>
+      <div id="divELEVEN">
+        <h1 id="h1ELEVEN">ELEVEN</h1>
       </div>
-      <div v-if="product">
-        <div id="PRODUTO">
-          <img :src="product.imagem" :alt="product.nome" id="imgproduto">
-          <div id="descprod">
-            <h2 id="h2Prod">{{ product.nome }}</h2>
-            <p id="pDesc">{{ product.descricao }}</p>
-            <h3 id="h3Preço">R$ {{ product.preco }}</h3>
-            <div class="containerquantidade">
-              <h2 id="Quant">Quantidade</h2>
-              <div id="subcont">
-                <button id="buttonDecrement" @click="decrementCounter">-</button>
-                <input type="text" :value="counterValue" disabled id="counterValue">
-                <button id="buttonIncrement" @click="incrementCounter">+</button>
-              </div>
-            </div>
-            <button id="botaoaddcar" @click="addToCart">ADICIONAR AO CARRINHO</button>
-          </div>
-        </div>
-        <div class="imagens">
-          <!-- Aqui você pode adicionar as imagens relacionadas a outros produtos -->
-        </div>
-      </div>
-      <div v-else>
-        <p>Nenhum produto encontrado.</p>
+      <div id="divCarrinho">
+        <button id="botaoimg" @click="goToCart">
+          <img id="pngtop" src="imagens/shoppingCarrinho.png">
+        </button>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        searchQuery: '',
-        product: null,
-        counterValue: 1,
+    <div id="line4"></div>
+    <div id="divsearch">
+      <input type="text" aria-label="Realizar Busca" autocomplete="off" name="Ntt" v-model="searchQuery" class="input" placeholder="O Que Você Procura ?">
+      <button id="searchButton" @click="searchProduct">Buscar</button>
+    </div>
+    <div v-if="product">
+      <div id="PRODUTO">
+        <img :src="product.imagem" :alt="product.nome" id="imgproduto">
+        <div id="descprod">
+          <h2 id="h2Prod">{{ product.nome }}</h2>
+          <p id="pDesc">{{ product.descricao }}</p>
+          <h3 id="h3Preço">R$ {{ product.preco }}</h3>
+          <div class="containerquantidade">
+            <h2 id="Quant">Quantidade</h2>
+            <div id="subcont">
+              <button id="buttonDecrement" @click="decrementCounter">-</button>
+              <input type="text" :value="counterValue" disabled id="counterValue">
+              <button id="buttonIncrement" @click="incrementCounter">+</button>
+            </div>
+          </div>
+          <button id="botaoaddcar" @click="addToCart(product)">ADICIONAR AO CARRINHO</button>
+        </div>
+      </div>
+      <div class="imagens">
+        <!-- Aqui você pode adicionar as imagens relacionadas a outros produtos -->
+      </div>
+    </div>
+    <div v-else>
+      <p>Nenhum produto encontrado.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      searchQuery: '',
+      product: null,
+      counterValue: 1,
+    };
+  },
+  methods: {
+    goBack() {
+      // Implemente a lógica para voltar à página anterior
+    },
+    goToCart() {
+      // Implemente a lógica para ir para a página do carrinho
+    },
+    incrementCounter() {
+      this.counterValue++;
+    },
+    decrementCounter() {
+      if (this.counterValue > 1) {
+        this.counterValue--;
+      }
+    },
+    searchProduct() {
+      // Realiza a requisição GET para obter os dados do produto
+      axios.get(`http://localhost:8080/api/produto/5`)
+        .then(response => {
+          this.product = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    addToCart(produto) {
+      const item = {
+        id: produto.id,
+        nome: produto.nome,
+        preco: produto.preco,
+        quantidade: this.counterValue,
+        subtotal: produto.preco * this.counterValue
       };
+
+      this.$emit('add-to-cart', item);
+
+      // Reinicializa a quantidade para 1
+      this.counterValue = 1;
+
+      // Exibe uma mensagem de sucesso ou redireciona para a página do carrinho
+      console.log(item);
+      alert("Produto adicionado ao carrinho com sucesso!");
     },
-    methods: {
-      goBack() {
-        // Implemente a lógica para voltar à página anterior
-      },
-      goToCart() {
-        // Implemente a lógica para ir para a página do carrinho
-      },
-      incrementCounter() {
-        this.counterValue++;
-      },
-      decrementCounter() {
-        if (this.counterValue > 1) {
-          this.counterValue--;
-        }
-      },
-      searchProduct() {
-        // Realiza a requisição GET para obter os dados do produto
-        axios.get(`http://localhost:8080/api/produto/3`)
-          .then(response => {
-            this.product = response.data;
-          })
-          .catch(error => {
-            console.error(error);
-          });
-      },
-      addToCart() {
-        // Implemente a lógica para adicionar o produto ao carrinho
-      },
-    },
-    mounted() {
-      this.searchProduct(); // Realiza a busca do produto ao carregar a página
-    },
-  };
-  </script>
-  
-  <style>
-  @import url('./productpage.css');
-  </style>
-  
+  },
+  mounted() {
+    this.searchProduct(); // Realiza a busca do produto ao carregar a página
+  },
+};
+</script>
+
+<style>
+@import './productpage.css';
+</style>
